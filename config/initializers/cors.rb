e8750a1a67d15846517fde9a024e68c275594da5
Mehmet <mehmetk6158@gmail.com>
2021-12-24 21:32:59 -0500

    allow netlify

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080', 'https://app.netlify.com/sites/walkeronrails'
+    origins 'http://localhost:8080', 'https://walkeronrails.netlify.app/'
 
     resource '*',
       headers: :any,
